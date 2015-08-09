7ff3c17242e970f2eff4ca2b1bf625a0d51a119a
WXLWEB <894147172@qq.com>
2015-08-09 17:04:18 +0800

    change logo

--- app/scripts/navbarScroll.js
+++ app/scripts/navbarScroll.js
@@ -7,11 +7,17 @@ $(document).ready(function() {
     t = document.documentElement.scrollTop || document.body.scrollTop;
     if (t > 0) {
       $('.navbar').removeClass('navbar-default').addClass('navbar-default-blue');
-      $('.navbar-brand').css('background', 'url("images/public/logo2.png") top left no-repeat');
+      $('.navbar-brand').css({
+        'background':'url("images/public/logo2.png") center center no-repeat',
+        'background-size':'cover'
+      });
       $('.marker').css('background-color', '#025499');
     } else {
       $('.navbar').removeClass('navbar-default-blue').addClass('navbar-default');
-      $('.navbar-brand').css('background', 'url("images/public/logo1.png") top left no-repeat');
+      $('.navbar-brand').css({
+        'background':'url("images/public/logo1.png") center center no-repeat',
+        'background-size':'cover'
+      });
       $('.marker').css('background-color', '#fff');
     }
   });
